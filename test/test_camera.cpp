80b3fa5f0e14067290974e1822ad2664249fec87
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2023-11-06 18:37:50 -0500

    documented test

--- test/test_camera.cpp
+++ test/test_camera.cpp
@@ -7,29 +7,33 @@ Ebb::Node *root;
 Ebb::Window *win;
 void frame_callback() {
     if (win->key_pressed(GLFW_KEY_ESCAPE)) {
-        win->close();
+        win->close();  // close the window when escape is pressed
     }
 
-    root->update();
-    win->clear(glm::vec3(1.0));
-    root->get_child<Ebb::Camera>()->get_tex()->draw();
+    root->update();  // update the node tree
+    win->clear(glm::vec3(1.0));  // clear the window
+    root->get_child<Ebb::Camera>()->get_tex()->draw();  // draw the camera's render texture to the window
 }
 
 int main(int argc, char **argv) {
-    win = new Ebb::Window(WIDTH, HEIGHT, "Camera test", &frame_callback);
+    win = new Ebb::Window(WIDTH, HEIGHT, "Camera test", &frame_callback);  // create a window
 
-    root = new Ebb::Node(nullptr);
+    root = new Ebb::Node(nullptr);  // root of the node tree
         // Multiplying width and height by 2 gives pretty cheap antialiasing (due to linear texture filtering from the camera texture to the screen)
-    Ebb::Camera *cam = new Ebb::Camera(root, WIDTH*2, HEIGHT*2, 60);
-    cam->transform.translate(glm::vec3(0, 0, -3));
-    Ebb::Mesh *obj = new Ebb::Mesh("models/suzanne.ebbm");
-    Ebb::Object *suzanne = new Ebb::Object(root);
+    Ebb::Camera *cam = new Ebb::Camera(root, WIDTH*2, HEIGHT*2, 60);  // create a camera (FOV 60)
+    cam->transform.translate(glm::vec3(0, 0, -3));  // move back
+    Ebb::Mesh *obj = new Ebb::Mesh("models/suzanne.ebbm");  // load in a  mesh
+    Ebb::Object *suzanne = new Ebb::Object(root);  // empty object to house suzanne
 
         // TODO: figure out why the light's z-axis is inverted
-    Ebb::Component *mesh = new Ebb::MeshRenderer(suzanne, new Ebb::Shaders::BlinnPhong(
+    Ebb::Component *mesh = new Ebb::MeshRenderer(suzanne, new Ebb::Shaders::BlinnPhong(  // mesh renderer for suzanne, with a shiny blue material
         glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(1.0f), glm::vec3(.1f),
         1.0f, 1.0f, glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(1.0f), 64.0f
     ), obj);
 
-    win->run();
+    root->setup();  // setup the node tree
+
+    win->run(); // run the windw
+
+    return 0; // exit
 }
