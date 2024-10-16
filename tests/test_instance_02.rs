888161a625430431fbca1493ea5af4ad1d052acd
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2024-10-15 21:21:27 -0400

    Add type aliases

--- tests/test_instance_02.rs
+++ tests/test_instance_02.rs
@@ -2,9 +2,9 @@ use ebb;
 
 fn main() {
     ebb::setup();
-    let mut instance: ebb::instance::Instance;
+    let mut instance: ebb::Instance;
 
-    let mut window = ebb::window::Window::new((1920, 1080), String::from("Ebb Test Window"), true);
+    let mut window = ebb::Window::new((1920, 1080), String::from("Ebb Test Window"), true);
     
     instance = ebb::create_instance!(window);
 
